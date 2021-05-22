37ef25e90655b59c92a6f9bfe3ec00d4f5956774
Cassio Zen <caantoni@microsoft.com>
2021-05-22 08:46:14 -0500

    Add dead experimentation code

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const input = "src/index.tsx";
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+];
+
+const plugins = [
+  typescript({
+    typescript: require("typescript"),
+  }),
+];
+
+export default [
+  {
+    input,
+    output: {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins,
+    external,
+  },
+  {
+    input,
+    output: {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    plugins,
+    external,
+  },
+];
