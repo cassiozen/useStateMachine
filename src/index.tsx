c05b513c921a9e7c11fb58103c3bddea49cfe816
Cassio Zen <caantoni@microsoft.com>
2021-06-14 07:40:20 -0500

    Should get context from Reducer

--- src/index.tsx
+++ src/index.tsx
@@ -233,8 +233,15 @@ function useStateMachineImpl<Context, Events>(context: Context): UseStateMachine
     };
 
     useEffect(() => {
-      const exit = config.states[machine.value]?.effect?.({ send, setContext, event: machine.event, context });
-      return typeof exit === 'function' ? () => exit({ send, setContext, event: machine.event, context }) : undefined;
+      const exit = config.states[machine.value]?.effect?.({
+        send,
+        setContext,
+        event: machine.event,
+        context: machine.context,
+      });
+      return typeof exit === 'function'
+        ? () => exit({ send, setContext, event: machine.event, context: machine.context })
+        : undefined;
       // We are bypassing the linter here because we deliberately want the effects to run:
       // - When the machine state changes or
       // - When a different event was sent (e.g. self-transition)
