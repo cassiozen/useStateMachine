d9faebe04641a692daafc31fdab39a1fa9a8629c
Cassio Zen <caantoni@microsoft.com>
2021-07-23 12:22:37 -0500

    Remove external logger file

--- src/logger.ts
+++
@@ -1,9 +0,0 @@
-export default function log(groupLabel: string, ...nestedMessages: [string, any][]) {
-  if (process.env.NODE_ENV === 'development') {
-    console.groupCollapsed('%cuseStateMachine', 'color: #888; font-weight: lighter;', groupLabel);
-    nestedMessages.forEach(message => {
-      console.log(message[0], message[1]);
-    });
-    console.groupEnd();
-  }
-}
