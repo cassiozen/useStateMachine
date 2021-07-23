54c3ff56f2ee1289f417ff15b48e7942e10843ee
Cassio Zen <caantoni@microsoft.com>
2021-07-23 14:50:16 -0500

    More flexible prettier

--- src/extras.ts
+++ src/extras.ts
@@ -1,40 +1,31 @@
 import { useRef } from "react";
 
 export const R = {
-  get: <R extends R.Unknown>(r: R, k: R.Key<R>) =>
-    (r as any)[k] as R.Value<R> | undefined,
-  concat: <R1 extends R.Unknown, R2 extends R.Unknown>(
-    r1: R1,
-    r2: R2
-  ) =>
-    ({ ...r1, ...r2 }) as any as R.Concat<R1, R2>,
-  fromMaybe: <R extends R.Unknown>(r: R | undefined) => r ?? {} as R,
-  keys: <R extends R.Unknown>(r: R) => Object.keys(r) as R.Key<R>[]
-}
+  get: <R extends R.Unknown>(r: R, k: R.Key<R>) => (r as any)[k] as R.Value<R> | undefined,
+  concat: <R1 extends R.Unknown, R2 extends R.Unknown>(r1: R1, r2: R2) =>
+    (({ ...r1, ...r2 } as any) as R.Concat<R1, R2>),
+  fromMaybe: <R extends R.Unknown>(r: R | undefined) => r ?? ({} as R),
+  keys: <R extends R.Unknown>(r: R) => Object.keys(r) as R.Key<R>[],
+};
 export namespace R {
-  export type Of<K extends string, V> =
-    { __V: V, __K: K }
-  
-  export type Unknown =
-  { __V: unknown, __K: string }
-  
-  export type Key<R extends R.Unknown> =
-    R extends { __K: infer K } ? K : never;
-  export type Value<R extends R.Unknown> =
-    R extends { __V: infer V } ? V : never
-  export type Concat<R1 extends R.Unknown, R2 extends R.Unknown> =
-    R.Of<
-      R.Key<R1> | R.Key<R2>,
-      R.Value<R1> | R.Value<R2>
-    >  
+  export type Of<K extends string, V> = { __V: V; __K: K };
+
+  export type Unknown = { __V: unknown; __K: string };
+
+  export type Key<R extends R.Unknown> = R extends { __K: infer K } ? K : never;
+  export type Value<R extends R.Unknown> = R extends { __V: infer V } ? V : never;
+  export type Concat<R1 extends R.Unknown, R2 extends R.Unknown> = R.Of<
+    R.Key<R1> | R.Key<R2>,
+    R.Value<R1> | R.Value<R2>
+  >;
 }
 
 export const useConstant = <T>(compute: () => T): T => {
   const ref = useRef<T | null>(null);
   if (ref.current === null) ref.current = compute();
   return ref.current;
-}
+};
 
 export const assertNever = (_value: never): never => {
-  throw new Error('Invariant: assertNever was called')
-}
+  throw new Error("Invariant: assertNever was called");
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,16 @@
 import { useEffect, useReducer } from "react";
 import { UseStateMachine, Machine } from "./types";
-import { assertNever, R, useConstant } from "./extras"
+import { assertNever, R, useConstant } from "./extras";
 
 const useStateMachineImpl = (definition: Machine.Definition.Impl) => {
-  const [state, dispatch] =
-    useReducer(
-      createReducer(definition),
-      createInitialState(definition)
-    )
+  const [state, dispatch] = useReducer(createReducer(definition), createInitialState(definition));
 
-  const send = useConstant(() =>
-    (sendable: Machine.Sendable.Impl) => dispatch({type: "SEND", sendable })
-  )
+  const send = useConstant(() => (sendable: Machine.Sendable.Impl) => dispatch({ type: "SEND", sendable }));
 
   const setContext = (updater: Machine.ContextUpdater.Impl) => {
     dispatch({ type: "SET_CONTEXT", updater });
     return { send };
-  }
+  };
 
   useEffect(() => {
     const entry = R.get(definition.states, state.value)!.effect;
@@ -25,51 +19,43 @@ const useStateMachineImpl = (definition: Machine.Definition.Impl) => {
       setContext,
       event: state.event,
       context: state.context,
-    })
+    });
 
     return typeof exit === "function"
       ? () => exit?.({ send, setContext, event: state.event, context: state.context })
       : undefined;
-
-  }, [state.value, state.event]); // eslint-disable react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.value, state.event]); 
 
   return [state, send];
 };
 
-const createInitialState = (definition: Machine.Definition.Impl): Machine.State.Impl =>
-  ({
-    value: definition.initial,
-    context: definition.context as Machine.Context.Impl,
-    event: undefined,
-    nextEvents:
-      R.keys(R.concat(
-        R.fromMaybe(R.get(definition.states, definition.initial)!.on),
-        R.fromMaybe(definition.on)
-      ))
-  })
+const createInitialState = (definition: Machine.Definition.Impl): Machine.State.Impl => ({
+  value: definition.initial,
+  context: definition.context as Machine.Context.Impl,
+  event: undefined,
+  nextEvents: R.keys(
+    R.concat(R.fromMaybe(R.get(definition.states, definition.initial)!.on), R.fromMaybe(definition.on))
+  ),
+});
 
 const createReducer = (definition: Machine.Definition.Impl) => {
   let log = createLogger(definition);
-  return (
-    machineState: Machine.State.Impl,
-    internalEvent: InternalEvent
-  ): Machine.State.Impl => {
+  return (machineState: Machine.State.Impl, internalEvent: InternalEvent): Machine.State.Impl => {
     if (internalEvent.type === "SET_CONTEXT") {
       let nextContext = internalEvent.updater(machineState.context);
-      log("Context update", ["Previous Context", machineState.context], ["Next Context", nextContext])
+      log("Context update", ["Previous Context", machineState.context], ["Next Context", nextContext]);
 
-      return { ...machineState, context: nextContext }
+      return { ...machineState, context: nextContext };
     }
 
     if (internalEvent.type === "SEND") {
       let sendable = internalEvent.sendable;
-      let event = typeof sendable === "string" ? { type: sendable } : sendable
+      let event = typeof sendable === "string" ? { type: sendable } : sendable;
       let context = machineState.context;
       let stateNode = R.get(definition.states, machineState.value)!;
       let resolvedTransition =
-        R.get(R.fromMaybe(stateNode.on), event.type) ??
-        R.get(R.fromMaybe(definition.on), event.type)
-
+        R.get(R.fromMaybe(stateNode.on), event.type) ?? R.get(R.fromMaybe(definition.on), event.type);
 
       if (!resolvedTransition) {
         log(
@@ -81,63 +67,59 @@ const createReducer = (definition: Machine.Definition.Impl) => {
       }
 
       let selectedTransition =
-        typeof resolvedTransition === "string" ? { target: resolvedTransition } :
-        resolvedTransition.guard === undefined ? { target: resolvedTransition.target } :
-        resolvedTransition.guard({ context, event }) ? { target: resolvedTransition.target } :
-        { target: resolvedTransition.target, isDenied: true };
+        typeof resolvedTransition === "string"
+          ? { target: resolvedTransition }
+          : resolvedTransition.guard === undefined
+          ? { target: resolvedTransition.target }
+          : resolvedTransition.guard({ context, event })
+          ? { target: resolvedTransition.target }
+          : { target: resolvedTransition.target, isDenied: true };
 
       if ("isDenied" in selectedTransition && selectedTransition.isDenied) {
         log(
           `Transition from "${machineState.value}" to "${selectedTransition.target}" denied by guard`,
           ["Event", event],
           ["Context", context]
-        )
+        );
         return machineState;
       }
-      log(`Transition from "${machineState.value}" to "${selectedTransition.target}"`, ['Event', event])
-      
+      log(`Transition from "${machineState.value}" to "${selectedTransition.target}"`, ["Event", event]);
+
       return {
         value: selectedTransition.target,
         context,
         event,
-        nextEvents:
-          R.keys(R.concat(
-            R.fromMaybe(stateNode.on),
-            R.fromMaybe(definition.on)
-          ))
-      }
+        nextEvents: R.keys(R.concat(R.fromMaybe(stateNode.on), R.fromMaybe(definition.on))),
+      };
     }
 
-    return assertNever(internalEvent)
+    return assertNever(internalEvent);
+  };
+};
+
+interface SetContextEvent {
+  type: "SET_CONTEXT";
+  updater: Machine.ContextUpdater.Impl;
+}
+
+interface SendEvent {
+  type: "SEND";
+  sendable: Machine.Sendable.Impl;
+}
+
+type InternalEvent = SetContextEvent | SendEvent;
+
+const createLogger = (definition: Machine.Definition.Impl) => (groupLabel: string, ...nested: [string, any][]) => {
+  if (!definition.verbose) return;
+  if (process.env.NODE_ENV === "development") {
+    console.groupCollapsed("%cuseStateMachine", "color: #888; font-weight: lighter;", groupLabel);
+    nested.forEach(message => {
+      console.log(message[0], message[1]);
+    });
+    console.groupEnd();
   }
 };
 
-interface SetContextEvent
-  { type: "SET_CONTEXT"
-  , updater: Machine.ContextUpdater.Impl
-  }
-
-interface SendEvent
-  { type: "SEND"
-  , sendable: Machine.Sendable.Impl
-  }
-
-type InternalEvent =
-  | SetContextEvent
-  | SendEvent
-
-const createLogger = (definition: Machine.Definition.Impl) =>
-  (groupLabel: string, ...nested: [string, any][]) => {
-    if (!definition.verbose) return;
-    if (process.env.NODE_ENV === "development") {
-      console.groupCollapsed("%cuseStateMachine", "color: #888; font-weight: lighter;", groupLabel);
-      nested.forEach(message => {
-        console.log(message[0], message[1]);
-      });
-      console.groupEnd();
-    }
-  }
-
-const useStateMachine = useStateMachineImpl as UseStateMachine
+const useStateMachine = useStateMachineImpl as UseStateMachine;
 export default useStateMachine;
-export const t = <T extends unknown>() => null as T
+export const t = <T extends unknown>() => null as T;
