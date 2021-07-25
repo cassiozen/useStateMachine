a4ddab012680725d36b460b86f18b73dd56766a5
Devansh Jethmalani <jethmalani.devansh@gmail.com>
2021-07-25 16:36:22 +0530

    use symbols for key and value of `R`

--- src/extras.ts
+++ src/extras.ts
@@ -5,15 +5,20 @@ export const R = {
   concat: <R1 extends R.Unknown, R2 extends R.Unknown>(r1: R1, r2: R2) =>
     (({ ...r1, ...r2 } as any) as R.Concat<R1, R2>),
   fromMaybe: <R extends R.Unknown>(r: R | undefined) => r ?? ({} as R),
-  keys: <R extends R.Unknown>(r: R) => Object.keys(r) as R.Key<R>[],
+  keys: <R extends R.Unknown>(r: R) => Object.keys(r) as R.Key<R>[]
 };
+
+const $$K = Symbol("R.$$K");
+const $$V = Symbol("R.$$V");
 export namespace R {
-  export type Of<K extends string, V> = { __V: V; __K: K };
+  export type $$K = typeof $$K;
+  export type $$V = typeof $$V;
 
-  export type Unknown = { __V: unknown; __K: string };
+  export type Of<K extends string, V> = { [$$K]: K, [$$V]: V };
+  export type Unknown = Of<string, unknown>
 
-  export type Key<R extends R.Unknown> = R extends { __K: infer K } ? K : never;
-  export type Value<R extends R.Unknown> = R extends { __V: infer V } ? V : never;
+  export type Key<R extends R.Unknown> = R[$$K]
+  export type Value<R extends R.Unknown> = R[$$V];
   export type Concat<R1 extends R.Unknown, R2 extends R.Unknown> = R.Of<
     R.Key<R1> | R.Key<R2>,
     R.Value<R1> | R.Value<R2>
