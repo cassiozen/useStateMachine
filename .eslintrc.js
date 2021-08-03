7f8b8ebe11ecf01d2fcc1710a2b4aa633207a663
Cassio Zen <cassio@hey.com>
2021-08-02 22:22:15 -0500

    Build

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app', 'prettier/@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ assets/js/343.0e2b42f1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkuse_state_machine=self.webpackChunkuse_state_machine||[]).push([[343],{3343:function(e,s,t){t.r(s)}}]);
